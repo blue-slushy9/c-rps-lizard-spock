4b0c6dbca004f9c1f40c69f60c10f461d72f0238
blue-slushy9 <ra.cruz971@gmail.com>
2023-12-29 14:45:46 -0800

    consolidated the two print functions into one

--- rps_lizard_spock.c
+++ rps_lizard_spock.c
@@ -3,34 +3,27 @@
 // You want to declare your functions you plan to use in your program as
 // function prototypes before you call them inside of main() or define them;
 
-// This is the introduction to the game, it is just text that will print to
-// the terminal;
-void intro();
-
-// These are the actual rules to the game, again it is simple text that will
-// print to the terminal;
+// These are the rules to the game, it is simple text that will print to the terminal;
 void rules();
 
 // main function will contain all other functions in this program;
 int main() {
     // Call the introduction function;
-    intro();
+    //intro();
     // Call the rules function;
     rules();
 
     return 0;
 }
 
-// Define intro function;
-void intro() {
-    printf("\nThis is Rock-Paper-Scissors-Lizard-Spock, from the hit TV show,\n"
-           "Big Bang Theory! It is similar to Rock-Paper-Scissors, but with\n"
-	   "two additional options: Lizard and Spock.\n\n");
-}
+
 
 // Define rules function;
 void rules() {
-    printf("Nobody can explain the rules like Sheldon Cooper can, so I will\n" 
+    printf("\nThis is Rock-Paper-Scissors-Lizard-Spock, from the hit TV show,\n"
+           "Big Bang Theory! It is similar to Rock-Paper-Scissors, but with\n"
+	   "two additional options: Lizard and Spock.\n\n"
+           "Nobody can explain the rules like Sheldon Cooper can, so I will\n" 
 	   "simply quote him: 'Scissors cuts paper, paper covers rock,\n" 
 	   "rock crushes lizard, lizard poisons Spock, Spock smashes scissors,\n" 
 	   "scissors decapitates lizard, lizard eats paper, paper disproves Spock,\n" 
