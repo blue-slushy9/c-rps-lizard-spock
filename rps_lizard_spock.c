9d92f7d589099d340508b3627ff03c9e95d4617a
blue-slushy9 <ra.cruz971@gmail.com>
2023-12-28 17:05:07 -0800

    made a lil progress

---
+++ rps_lizard_spock.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main() {
+	printf("This is Rock-Paper-Scissors-Lizard-Spock, from the hit TV show, Big\n"
+		"Bang Theory! It is similar to Rock-Paper Scissors, but with\n"
+		"two additional options: Lizard and Spock.\n");
+
+        return 0;
+}
+
