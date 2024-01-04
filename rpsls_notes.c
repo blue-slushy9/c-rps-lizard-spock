06552ce5f131ce4e714590ce21bcadf2ae027f3d
blue-slushy9 <ra.cruz971@gmail.com>
2024-01-04 15:53:31 -0800

    added some notes about pointers and address-of operators

---
+++ rpsls_notes.c
@@ -0,0 +1,19 @@
+* is the dereference operator, it is used to access the value stored at the address
+held by a pointer;
+
+& is the address-of operator, it returns the memory address of a variable;
+
+// DEREFERENCE OPERATOR * 
+takes you to the value which the pointer references; 
+
+// ADDRESS-OF &
+brings you the memory address of a variable;
+
+// EXAMPLES
+
+// &rpslsDict retrieves the memory address of the variable, rpslsDict, which is then 
+// used to print the dictionary, rpslsDict;
+printDict(&rpslsDict);
+
+
+ 
