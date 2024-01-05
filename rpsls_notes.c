0d6c61bcee82bc4c161345564e85be0165f964b9
blue-slushy9 <ra.cruz971@gmail.com>
2024-01-04 16:11:36 -0800

    more point and address-of operator notes

--- rpsls_notes.c
+++ rpsls_notes.c
@@ -1,13 +1,17 @@
-* is the dereference operator, it is used to access the value stored at the address
-held by a pointer;
+// * is the dereference operator, it is used to access the value stored at the address
+// held by a pointer;
 
-& is the address-of operator, it returns the memory address of a variable;
+// & is the address-of operator, it returns the memory address of a variable;
 
 // DEREFERENCE OPERATOR * 
-takes you to the value which the pointer references; 
+// takes you to the value which the pointer references; 
+
+// When you DEREFERENCE a pointer, you are asking the program to go to the memory
+// location specified by the pointer and retrieve the value stored there;
 
 // ADDRESS-OF &
-brings you the memory address of a variable;
+// brings you the memory address of a variable;
+
 
 // EXAMPLES
 
@@ -16,4 +20,8 @@ brings you the memory address of a variable;
 printDict(&rpslsDict);
 
 
- 
+// Declare a pointer and intialize it with the address of x;
+int *ptr = &x;
+
+// Dereference the pointer to access the value at the memory address it points to;
+int value = *ptr;
