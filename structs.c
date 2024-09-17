564b639c087301252fed5489b7388843c43274e9
blue-slushy9 <ra.cruz971@gmail.com>
2024-09-16 19:29:26 -0700

    Continued working on structs.c to help me understand how to create and work with structs. I created three separate structs, one for each layer of my nested dictionary, will continue building and testing them next time to make sure I am doing everything right.

--- structs.c
+++ structs.c
@@ -8,20 +8,33 @@
 // e.g. 'Lizard poisons Spock...'
 #define MAX_VALUE_LENGTH 50
 
-//#define 
+// Max entries for the middle dictionary
+#define MAX_MID_ENTRIES 5
+
+// Max entries for the inner dictionary
+#define MAX_INNER_ENTRIES 10
 
 /* The typedef keyword is used to create a new type alias. It allows you to 
 give a name (alias) to a data type so that you can use it without repeating 
 the full definition of the type. In this case, it's creating a type called 
 InnerDict that represents the structure. */
 
-// Define structure for inner dictionary
+// Define structure for inner dictionary, used for a single entry
 typedef struct {
     char key[MAX_KEY_LENGTH];
     char value[MAX_VALUE_LENGTH];
-} InnerDict;
+} InnerDictEntry;
+
+// Define structure for middle dictionary, used for a single entry
+typedef struct {
+    char key[MAX_KEY_LENGTH];
+    // This array holds the inner dictionary
+    InnerDictEntry entries[MAX_INNER_ENTRIES];
+} MiddleDictEntry;
 
 // Define structure for outer dictionary
 typedef struct {
-    
+    char key[MAX_KEY_LENGTH];
+    // This array holds the middle dictionary
+    MiddleDictEntry entries[MAX_MID_ENTRIES];
 } OuterDict;
